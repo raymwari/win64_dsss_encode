e648c651410f82f6d9027c6408aa325f1a085b7d
raymwari <mwari.ray@gmail.com>
2025-07-10 22:38:08 -0700

    Add files via upload

---
+++ src/enc.asm
@@ -0,0 +1,107 @@
+section .data
+  spread_factor equ 5
+  seed equ 0x2000
+  modulus equ 6656
+  m_const equ 101
+  a_const equ 17 
+  efile_name db "embed.txt", 0
+  OF_PROMPT equ 0x00002000
+  secret_cap equ 36
+  header_offet equ 192
+
+section .bss
+  spread_pos resb 1
+  efile_buf resb 136
+  secret resb secret_cap
+  read resb secret_cap
+  secret_bin resb 240
+
+section .text
+  extern ExitProcess, GetStdHandle, WriteConsoleA
+  extern OpenFile, ReadFile, CloseHandle
+  extern bin_buf, out, ecode
+  global _main
+  enc:
+    mov rcx, efile_name
+    lea rdx, efile_buf
+    mov r8, OF_PROMPT
+    call OpenFile
+    mov rsi, rax
+    mov rcx, rsi
+    lea rdx, secret
+    mov r8, secret_cap
+    lea r9, read
+    mov byte [rsp + 32], 0
+    call ReadFile
+    test rax, rax 
+    jz ecode
+    mov rcx, rsi
+    call CloseHandle
+    test rax, rax 
+    jz ecode
+
+    mov rsi, secret
+    mov rbx, secret_bin
+    mov rdx, [read]
+    bytewise:
+      mov rax, [rsi]
+      mov rcx, 8
+
+      bitwise:
+        shl al, 1
+        jnc write_null
+        mov byte [rbx], '1'
+        jmp next_bit
+
+      write_null:
+        mov byte [rbx], '0'
+
+      next_bit:
+        inc rbx
+        dec rcx
+        cmp rcx, 0
+        jne bitwise
+    
+    inc rsi
+    dec rdx
+    cmp rdx, 0
+    jne bytewise
+
+    mov rsi, secret_bin
+    mov rcx, [read]
+    imul rcx, 8
+    mov r11, 0
+
+    mov rdi, spread_pos
+    mov r8, modulus
+    mov r12, bin_buf
+    toencloop:
+      mov r9, spread_factor
+      modulate:
+        mov r10, seed
+        add r10, r11
+        imul r10, m_const
+        add r10, a_const
+        mov rax, r10
+        xor rdx, rdx
+        div r8
+        mov r13b, [rsi]
+        cmp rdx, header_offet
+        jge skip
+        add rdx, header_offet
+        
+        skip:
+          mov byte [r12 + rdx], r13b
+          xor rdx, rdx
+          dec r9
+          cmp r9, 0
+          jne modulate      
+
+      inc r11
+      inc rsi
+      dec rcx
+      cmp rcx, 0
+      jne toencloop
+
+    pop rsi
+    jmp out
---
+++ src/main.asm
@@ -0,0 +1,130 @@
+section .data
+  file_name db "in.mp3", 0
+  OF_PROMPT equ 0x00002000
+  MAX_JPEG_SIZE equ 966656
+  MAX_BIN_SIZE equ 9666560
+  bin_f db "out.mp3", 0
+
+section .bss
+  ofstruct resb 136
+  file_read_buf resb MAX_JPEG_SIZE
+  read resb 12
+  bin_buf resb MAX_BIN_SIZE
+
+section .text
+  extern OpenFile, ExitProcess
+  extern ecode, enc
+  extern ReadFile, sig
+  extern WriteFile
+  global _main
+  _main:
+    push rbp
+    sub rsp, 40
+
+    lea rcx, file_name
+    lea rdx, ofstruct
+    mov r8d, OF_PROMPT
+    call OpenFile
+    test rax, rax
+    jz ecode
+    mov rsi, rax
+    jmp fileop
+    end:
+      add rsp, 40
+      mov rcx, 0
+      call ExitProcess
+  
+  fileop:
+    mov rcx, rsi 
+    push rcx
+    lea rdx, file_read_buf
+    mov r8, MAX_JPEG_SIZE
+    lea r9, read
+    mov qword [rsp + 32], 0
+    call ReadFile
+    test rax, rax 
+    jz ecode
+
+    mov rdi, file_read_buf
+    mov rdx, [read] 
+    
+    mov r8, bin_buf
+    binloop:
+      mov byte al, [rdi]
+      mov rcx, 8  
+      nextbit:
+        test rcx, rcx
+        jz nextbyte
+
+        shl al, 1
+        jc write_one 
+        mov byte [r8], '0'
+        inc r8
+        dec rcx
+        jmp nextbit
+
+        write_one:
+          mov byte [r8], '1'
+          inc r8          
+          dec rcx
+          jmp nextbit
+
+      nextbyte:
+        inc rdi
+        dec rdx
+        cmp rdx, 0
+        jne binloop
+
+    mov r8, file_read_buf
+    mov rcx, [read]
+    clean_buf:
+      mov byte [r8], 0
+      inc r8
+      dec rcx
+      cmp rcx, 0
+      jne clean_buf
+
+    jmp enc
+    out:
+      mov r8, bin_buf
+      mov r9, file_read_buf
+      mov rdx, [read] 
+      unbinloop:
+        mov al, 0
+        mov rcx, 8
+
+        reconstruct:
+          mov bl, [r8]
+          shl al, 1
+          cmp bl, '1'
+          jne write_zero
+          or al, 1
+        
+        write_zero:
+          inc r8
+          dec rcx
+          cmp rcx, 0
+          jne reconstruct
+      
+        mov [r9], al
+        inc r9
+        dec rdx
+        cmp rdx, 0
+        jne unbinloop
+
+      mov rcx, bin_f
+      lea rdx, ofstruct
+      mov r8d, 0x00001000
+      call OpenFile
+      test rax, rax
+      jz ecode
+    
+      mov rcx, rax
+      lea rdx, file_read_buf
+      mov r8d, [read]
+      mov r9d, 0
+      mov qword [rsp + 32], 0
+      call WriteFile
+      test rax, rax
+      jz ecode    
+      jmp end
